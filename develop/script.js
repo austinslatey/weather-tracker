9010b1f77cde0cf9db3f66f56d02cd1a29d2d536
austinslatey <93884283+austinslatey@users.noreply.github.com>
2022-01-07 16:43:36 -0600

    added basic layout for the project

---
+++ develop/script.js
@@ -0,0 +1,10 @@
+//get a weather api
+// get location api
+//format for a current time and day
+//create functions based on what is needed for api to run
+    // search for a location desired and display
+     // weather forcast display for location 
+     // search history
+        //
+     //  on click functions for search button
+     // 

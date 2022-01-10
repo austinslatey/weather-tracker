647b1dae78923d8cfbbc3a0ccc228512fa1bccf6
Mr WhiteKidd <austinslater58@gmail.com>
2022-01-09 23:26:47 -0600

    able to search daily weather for a city

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,15 @@
-var searchFormEl = document.querySelector("#search-form");
-var cityInputEl = document.querySelector("#city-name")
-var searchButtonEl = document.querySelector("#search-btn");
+var fetchButton = document.querySelector('.button');
+var cityInput = document.querySelector(".city-name");
+var cityName = document.querySelector(".display-name");
+var name = document.querySelector(".name");
+var description = document.querySelector(".desc");
+var temp = document.querySelector(".temp");
+
+var list = document.querySelector("ul")
 
 //get a weather api
-var apiKey = "7f6e2d150ab435b458c12a5ccbabd3de";
-//ask for location
-if (navigator.geolocation) {
-   navigator.geolocation.getCurrentPosition(function(pos) {
-      var geoLat = pos.coords.latitude.toFixed(5);
-      var geoLong = pos.coords.longitude.toFixed(5);
-      var geoAcc = pos.coords.accuracy.toFixed(1);
-
-   });
-} else {
-   alert("geoloc broken");
-}
+var apiKey = '7f6e2d150ab435b458c12a5ccbabd3de';
+//ask for location\
 
 //format for a current time and day
 var todaysDate = moment().format("MMM Do YY");
@@ -22,61 +17,96 @@ var cities = [""];
 var recentlySearched = {};
 
 //create functions based on what is needed for api to run
-   //initial pull for city name given lat and long
-var buttonSubmit = function(event) {
+
+fetchButton.addEventListener('click', function(event) {
    event.preventDefault();
+   var getApi = function() {
+      //api url
+      var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+cityInput.value+'&cnt=7&units=imperial&appid=' + apiKey;
+      console.log(apiUrl);
+      //make a request to url
+      fetch(apiUrl)
+         .then(function(response) {
+            //request succeded
+            if (response.ok) {
+               response.json().then(function(data) {
+                  console.log(data);
+                  var nameValue = data['name'];
+                  var tempValue = data['main']['temp'];
+                  var descValue = data['weather'][0]['description'];
+                  var getId = data['sys']['id'];
+                  console.log(getId);
+                  var lat = data['coord']['lat'];
+                  var long = data['coord']['lon'];
+                  console.log(lat);
+                  console.log(long);
+                  var grabLat = lat;
+                  console.log(grabLat);
+                  var grabLong = long;
+                  console.log(grabLong);
+
+                  cityName.innerHTML = nameValue;
+                  temp.innerHTML = tempValue;
+                  description.innerHTML = descValue;
+                  
+                  
+                  
+                  
+               });
+            } 
+         })
+         .catch(function(error) {
+            alert("Unable to connect to Weather-Tracker");
+         });
+      }
 
    //get value from input element
-   var cityName = cityInputEl.value.trim();
+   var cityName = cityInput.value.trim();
    var citySplit = cityName.split(" ");
    var cityNoSpace = citySplit.join("");
    console.log(cityName);
    console.log(citySplit);
    console.log(cityNoSpace);
 
+
    if (cityName) {
-      getCityId(cityName);
+      getApi(cityName);
 
    } else {
-      alert("Please enter a valid city");
+      alert("Please enter a valid city")
    }
-}
+});
 
 
-   //initial pull for city name given lat and long
-   var getCityId = function(name) {
-      var key = '7f6e2d150ab435b458c12a5ccbabd3de'
-      var apiUrl= 'https://api.openweathermap.org/data/2.5/weather?q=minneapolis&appid=' + key;
-      console.log(apiUrl);
 
-      fetch(apiUrl)
-      .then(function(response) {return response.json() 
+
+//initial pull for city name given lat and long
+var oneCallID = function() {
+   apiId = '214caed1da037973b02525f881263cfe';
+   //api url
+   var newUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+grabLat+'&lon='+grabLong+'&appid='+apiKey;
+   //make a request to url
+   fetch(newUrl)
+      .then(function(response) {
+         //request succeded
+         if (response.ok) {
+            response.json().then(function(data) {
+               console.log(data);
+               
+            });
+         } 
       })
-      .then(function(data) {
-         var temp = data.main;
-         var place = data.name;
-         var description = data.weather[0];
-         var { sunrise, sunset } = data.sys;
-
-         var convertToFahrenheit = (temp * 9) / 5 + 32;
-         console.log(data);
-      })
-      .catch(function() {
-   
+      .catch(function(error) {
+         alert("Unable to connect to Weather-Tracker");
       });
    }
 
-   
-   window.onload = function() {
-      getCityId();
-   }
-
- 
+   oneCallID();
 
    
-    // search for a location desired and display
-     // weather forcast display for location 
-     // search history
-    //
-     //  on click functions for search button
-     // 
+  
+   //atempting to pull by city name 
+  
+
+
+
